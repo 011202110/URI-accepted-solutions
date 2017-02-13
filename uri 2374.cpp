2f57fdf9e8e8df64b2ce3b5d969773b970bcf4b7
Arif Khan <arifkhanshubro@gmail.com>
2017-02-14 01:38:50 +0600

    Add files via upload

---
+++ uri 2374.cpp
@@ -0,0 +1,15 @@
+/*                                     Codeman
+                                   Arif Khan Nihar
+                                   Uri 2374 - Pneu
+
+                                 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      int n ,m ;
+      cin >> n >> m;
+      cout << ((n != m)? n-m:0)<<endl;
+    return 0;
+}
