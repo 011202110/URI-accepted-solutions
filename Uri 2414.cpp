e7a23f42766ff7eafed02ed91cf8cb7815ae8147
Arif Khan <arifkhanshubro@gmail.com>
2017-02-20 03:52:39 +0600

    Add files via upload

---
+++ Uri 2414.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,ans = 0;
+
+   while(cin >> a and a != 0){
+   	ans = max(ans,a);
+   }
+    cout << ans << endl;
+
+    return 0;
+}
+
