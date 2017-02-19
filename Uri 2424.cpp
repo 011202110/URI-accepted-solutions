a2a4ee8038d6e082ee370bdf2a3e38a6e6da8331
Arif Khan <arifkhanshubro@gmail.com>
2017-02-20 03:06:25 +0600

    Add files via upload

---
+++ Uri 2424.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    cin >> a >> b ;
+     if(a >= 0 and a <= 432 and b >= 0 and b <= 468)cout << "dentro" << endl;
+     else cout << "fora" << endl;
+
+    return 0;
+}
+
