befcf4644f0790958245bbec3707692aca6a4a66
Arif Khan <arifkhanshubro@gmail.com>
2017-03-27 04:15:29 +0600

    Add files via upload

---
+++ Uri 2510.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int n;
+  string s;
+  cin >> n;
+  while(n--){
+      cin >> s;
+      cout << ((s == "Pistoleiro")? "Y" : "N") << endl;
+  }
+
+    return 0;
+}
